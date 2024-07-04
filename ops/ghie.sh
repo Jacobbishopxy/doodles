2bc66f3bb1636672bc083990bd87290738a01e3e
jacob <jacobbishopxy@gmail.com>
2024-07-04 21:51:17 +0800

    :hatching_chick: docker postgres

---
+++ ops/ghie.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# author:	Jacob Xie
+# date:	2024/07/04 21:40:18 Thursday
+# brief:
+
+gen-hie > hie.yaml
---
+++ ops/setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# author:	Jacob Xie
+# date:	2024/07/04 21:18:04 Thursday
+# brief:
+
+docker-compose down
+docker-compose up -d
