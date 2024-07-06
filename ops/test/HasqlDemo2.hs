88e86ca13edc702d3bb156bcdb4e2e51592071a7
jacob <jacobbishopxy@gmail.com>
2024-07-07 00:18:51 +0800

    :hatching_chick: `HasqlDemo2`

--- ops/test/HasqlDemo2.hs
+++ ops/test/HasqlDemo2.hs
@@ -8,29 +8,49 @@
 module Main where
 
 import Contravariant.Extras.Contrazip
-import Data.ByteString
+import Data.ByteString (ByteString)
 import Data.Int (Int32)
-import Data.Text
-import Data.Vector
+import qualified Data.List as List
+import Data.Text (Text)
+import Data.Vector (Vector, fromList, toList)
+import qualified Data.Vector as V
 import qualified Hasql.Connection as C
 import qualified Hasql.Decoders as D
 import qualified Hasql.Encoders as E
-import Hasql.Session (Session, run, statement)
-import Hasql.Statement (Statement (..))
+import qualified Hasql.Session as R
+import qualified Hasql.Statement as S
+import qualified Hasql.Transaction as T
+import qualified Hasql.Transaction.Sessions as TS
 
 ----------------------------------------------------------------------------------------------------
 -- Statements
 ----------------------------------------------------------------------------------------------------
 
-findUserByEmail :: Statement Text (Maybe Int32)
+-- `Session` 是在一次连接（查询）的上下文中执行的一批操作；
+-- `Statement` 是严格的单语句查询的规范，可以被参数化以及提前预备（如何进行查询）；
+-- `Statement` 由 SQL 模板，入参编码，返回值解码，以及是否准备好的标记所构成；
+-- `statement` 通过 `Statement` 与入参创建一个 `Session`；
+-- `run` 在提供的连接上，执行一批命令（statements）
+cleanUp :: C.Connection -> IO (Either R.SessionError ())
+cleanUp connection = R.run cleanUpSession connection
+  where
+    cleanUpSession :: R.Session ()
+    cleanUpSession = R.statement () cleanUpStatement
+    cleanUpStatement :: S.Statement () ()
+    cleanUpStatement = S.Statement rawSql E.noParams D.noResult True
+    rawSql = "truncate user, notification, notification_user"
+
+-- 根据 Email 查找用户
+findUserByEmail :: S.Statement Text (Maybe Int32)
 findUserByEmail =
-  Statement
+  S.Statement
     "select id from user where email = $1"
     (E.param $ E.nonNullable E.text)
     (D.rowMaybe $ D.column $ D.nonNullable D.int4)
     True
 
-insertUser :: Statement (Text, ByteString, Text, Maybe Text) Int32
+-- 插入用户
+insertUser :: S.Statement (Text, ByteString, Text, Maybe Text) Int32
 insertUser =
   let s =
         "insert into user (email, password, name, phone) \
@@ -44,9 +64,10 @@ insertUser =
           (E.param $ E.nullable E.text)
       decoder =
         D.singleRow $ (D.column . D.nonNullable) D.int4
-   in Statement s encoder decoder True
+   in S.Statement s encoder decoder True
 
-authenticateUser :: Statement (Text, ByteString) (Maybe (Bool, Int32))
+-- 通过 Email 与 password 验证用户
+authenticateUser :: S.Statement (Text, ByteString) (Maybe (Bool, Int32))
 authenticateUser =
   let s = "select password = $2, id from user where email = $1"
       encoder =
@@ -58,9 +79,10 @@ authenticateUser =
           (,)
             <$> D.column (D.nonNullable D.bool)
             <*> D.column (D.nonNullable D.int4)
-   in Statement s encoder decoder True
+   in S.Statement s encoder decoder True
 
-getUserDetails :: Statement Int32 (Maybe (Text, Text, Maybe Text, Bool))
+-- 获取用户明细
+getUserDetails :: S.Statement Int32 (Maybe (Text, Text, Maybe Text, Bool))
 getUserDetails =
   let s = "select name, email, phone, admin from user where id = $1"
       encoder = E.param $ E.nonNullable E.int4
@@ -71,31 +93,12 @@ getUserDetails =
             <*> D.column (D.nonNullable D.text)
             <*> D.column (D.nullable D.text)
             <*> D.column (D.nonNullable D.bool)
-   in Statement s encoder decoder True
+   in S.Statement s encoder decoder True
 
-getUserNotifications :: Statement Int32 (Vector (Int32, Text, Bool))
-getUserNotifications =
-  let s = "select id, message, read from notification where user = $1"
-      encoder = E.param $ E.nonNullable E.int4
-      decoder =
-        D.rowVector $
-          (,,)
-            <$> D.column (D.nonNullable D.int4)
-            <*> D.column (D.nonNullable D.text)
-            <*> D.column (D.nonNullable D.bool)
-   in Statement s encoder decoder True
-
-markNotificationRead :: Statement Int32 Bool
-markNotificationRead =
-  Statement
-    "update notification set read = true where id = $1"
-    (E.param $ E.nonNullable E.int4)
-    ((> 0) <$> D.rowsAffected)
-    True
-
-insertNotification :: Statement (Int32, Text) Int32
+-- 插入通知
+insertNotification :: S.Statement (Int32, Text) Int32
 insertNotification =
-  Statement
+  S.Statement
     "insert into notification (user, message, read)\
     \values ($1, $2, 'false')\
     \returning id"
@@ -106,49 +109,116 @@ insertNotification =
     encoder = contrazip2 (E.param $ E.nonNullable E.int4) (E.param $ E.nonNullable E.text)
     decoder = D.singleRow (D.column $ D.nonNullable D.int4)
 
+-- 获取通知
+getNotifications :: S.Statement Int32 (Vector (Int32, Text, Bool))
+getNotifications =
+  let s = "select id, message, read from notification where user = $1"
+      encoder = E.param $ E.nonNullable E.int4
+      decoder =
+        D.rowVector $
+          (,,)
+            <$> D.column (D.nonNullable D.int4)
+            <*> D.column (D.nonNullable D.text)
+            <*> D.column (D.nonNullable D.bool)
+   in S.Statement s encoder decoder True
+
+-- 将通知设为已读
+markNotificationRead :: S.Statement Int32 Bool
+markNotificationRead =
+  S.Statement
+    "update notification set read = true where id = $1"
+    (E.param $ E.nonNullable E.int4)
+    ((> 0) <$> D.rowsAffected)
+    True
+
+-- 将多个通知设为已读
+markNotificationsRead :: S.Statement (Vector Int32) (Vector Int32)
+markNotificationsRead =
+  S.Statement
+    "update notification set read = true where id = any ($1) returning id"
+    (E.param $ E.nonNullable $ E.array $ E.dimension List.foldl' (E.element $ E.nonNullable E.int4))
+    (D.rowVector $ D.column (D.nonNullable D.int4))
+    True
+
 ----------------------------------------------------------------------------------------------------
 -- Transactions
 ----------------------------------------------------------------------------------------------------
 
--- tRegister :: Text -> ByteString -> Text -> Maybe Text -> T.Transaction (Bool, Int32)
--- tRegister email password name phone = session TS.Write TS.Serializable $ do
---   possibleExistingId <- statement email findUserByEmail
---   case possibleExistingId of
---     Just existingId -> return (False, existingId)
---     Nothing -> do
---       newId <- statement (email, password, name, phone) insertUser
---       return (True, newId)
+-- 注册用户
+-- 已存在的 Email 不可注册用户
+registerUser :: Text -> ByteString -> Text -> Maybe Text -> T.Transaction (Bool, Int32)
+registerUser email password name phone = do
+  possibleExistingId <- tFindUserByEmail email
+  case possibleExistingId of
+    Just existingId -> return (False, existingId)
+    Nothing -> c >>= \i -> return (True, i)
+  where
+    c :: T.Transaction Int32
+    c = T.statement (email, password, name, phone) insertUser
 
--- tRegister' :: Text -> ByteString -> Text -> Maybe Text -> T.Transaction (Bool, Int32)
--- tRegister' email password name phone =
---   fromMaybeS
---     (fmap (\newId -> (True, newId)) (tInsertUser email password name phone))
---     (fmap (fmap (\existingId -> (False, existingId))) (tFindUserByEmail email))
+-- 推送通知
+-- 不存在的 Email 不可推送
+pushUserNotification :: Text -> Text -> T.Transaction Bool
+pushUserNotification email notification = do
+  possibleExistingId <- tFindUserByEmail email
+  case possibleExistingId of
+    Just existingId -> c existingId >> return True
+    Nothing -> return False
+  where
+    c :: Int32 -> T.Transaction Int32
+    c i = T.statement (i, notification) insertNotification
 
--- tFindUserByEmail :: Text -> T.Transaction (Maybe Int32)
--- tFindUserByEmail  = undefined
+-- 用户通知已读
+markUserNotificationsRead :: Text -> [Int32] -> T.Transaction [Int32]
+markUserNotificationsRead email notificationIds = do
+  possibleExistingId <- tFindUserByEmail email
+  case possibleExistingId of
+    Just _ -> c >>= \i -> return $ toList i
+    Nothing -> return []
+  where
+    c :: T.Transaction (Vector Int32)
+    c = T.statement (fromList notificationIds) markNotificationsRead
 
--- tInsertUser :: Text -> ByteString -> Text -> Maybe Text -> T.Transaction Int32
--- tInsertUser email password name phone = undefined
+----------------------------------------------------------------------------------------------------
+
+-- 带有 transaction 的查找用户
+tFindUserByEmail :: Text -> T.Transaction (Maybe Int32)
+tFindUserByEmail email = T.statement email findUserByEmail
 
 ----------------------------------------------------------------------------------------------------
 -- Session
+-- 执行函数，业务意义
 ----------------------------------------------------------------------------------------------------
 
-authenticate' :: Text -> ByteString -> Session (Maybe (Bool, Int32))
-authenticate' email password = statement (email, password) authenticateUser
+authenticate :: Text -> ByteString -> R.Session (Maybe (Bool, Int32))
+authenticate email password = R.statement (email, password) authenticateUser
 
-register :: Text -> ByteString -> Text -> Maybe Text -> Session (Bool, Int32)
-register = undefined
+register :: Text -> ByteString -> Text -> Maybe Text -> R.Session (Bool, Int32)
+register email password name phone =
+  TS.transaction
+    TS.RepeatableRead
+    TS.Write
+    (registerUser email password name phone)
 
-getUserDetails' :: Int32 -> Session (Maybe (Text, Text, Maybe Text, Bool))
-getUserDetails' userId = statement userId getUserDetails
+getDetails :: Int32 -> R.Session (Maybe (Text, Text, Maybe Text, Bool))
+getDetails userId = R.statement userId getUserDetails
 
-getNotifications :: Int32 -> Session (Vector (Int32, Text, Bool))
-getNotifications userId = statement userId getUserNotifications
+pushNotification :: Text -> Text -> R.Session Bool
+pushNotification email notification =
+  TS.transaction
+    TS.ReadCommitted
+    TS.Write
+    (pushUserNotification email notification)
 
-markNotificationRead' :: Int32 -> Session Bool
-markNotificationRead' notificationId = statement notificationId markNotificationRead
+getAllNotifications :: Int32 -> R.Session (Vector (Int32, Text, Bool))
+getAllNotifications userId = R.statement userId getNotifications
+
+markNotifications :: Text -> [Int32] -> R.Session [Int32]
+markNotifications email notificationIds =
+  TS.transaction
+    TS.RepeatableRead
+    TS.Write
+    (markUserNotificationsRead email notificationIds)
 
 ----------------------------------------------------------------------------------------------------
 -- Main
@@ -157,7 +227,51 @@ markNotificationRead' notificationId = statement notificationId markNotification
 main :: IO ()
 main = do
   Right connection <- C.acquire cfg
-  result <- run (authenticate' "foo@gmail.com" "abc123") connection
-  print result
-  where
-    cfg = C.settings "localhost" 5432 "hasql" "hasql" "postgres"
+
+  let uEmail = "foo@gmail.com"
+      uPswd = "abc123"
+      uName = "foo"
+      uPhone = "360100100"
+
+  -- 尝试验证一个用户，发现该用户并未被注册
+  res1 <- R.run (authenticate uEmail uPswd) connection
+  print res1
+
+  -- 注册用户
+  res2 <- R.run (register uEmail uPswd uName (Just uPhone)) connection
+  -- 获取用户注册结果
+  id' <- case res2 of
+    Right (t, i) | t == True -> do
+      putStrLn $ "user id " <> show i
+      return i
+    _ -> error "failed to register the user!"
+
+  -- 通过 id 查找用户明细
+  res3 <- R.run (getDetails id') connection
+  case res3 of
+    Right (Just d) -> print d
+    _ -> error "failed to get the user detail!"
+
+  -- 向用户推送通知
+  _ <- R.run (pushNotification uEmail "Hello world") connection
+  _ <- R.run (pushNotification uEmail "Hello Jacob") connection
+
+  -- 通过 id 获取用户通知列表
+  res4 <- R.run (getAllNotifications id') connection
+  print res4
+
+  -- 获取所有通知 ids
+  nIds <- case res4 of
+    Right v -> return $ V.toList $ V.map (\(x, _, _) -> x) v
+    _ -> return []
+
+  -- 将所有通知设为已读
+  res5 <- R.run (markNotifications uEmail nIds) connection
+  print res5
+
+  putStrLn "done"
+
+----------------------------------------------------------------------------------------------------
+
+cfg :: C.Settings
+cfg = C.settings "localhost" 5432 "hasql" "hasql" "postgres"
--- ops/test/HasqlDemo3.hs
+++ ops/test/HasqlDemo3.hs
@@ -15,12 +15,12 @@ import Control.Monad (join, replicateM_)
 import Data.Int (Int64)
 import Data.Scientific (Scientific)
 import Debug.Trace (traceShowM)
-import qualified Hasql.Connection as Connection
+import qualified Hasql.Connection as C
 import qualified Hasql.Decoders as D
 import qualified Hasql.Encoders as E
-import qualified Hasql.Session as Session
-import qualified Hasql.Statement as Statement
-import qualified Hasql.Transaction as Transaction
+import qualified Hasql.Session as R
+import qualified Hasql.Statement as S
+import qualified Hasql.Transaction as T
 import qualified Hasql.Transaction.Sessions as TS
 import System.Exit (exitFailure, exitSuccess)
 
@@ -28,41 +28,27 @@ import System.Exit (exitFailure, exitSuccess)
 -- Statements
 ----------------------------------------------------------------------------------------------------
 
--- `Session` 是在一次连接（查询）的上下文中执行的一批操作；
--- `Statement` 是严格的单语句查询的规范，可以被参数化以及提前预备（如何进行查询）；
--- `Statement` 由 SQL 模板，入参编码，返回值解码，以及是否准备好的标记所构成；
--- `statement` 通过 `Statement` 与入参创建一个 `Session`；
--- `run` 在提供的连接上，执行一批命令（statements）
-cleanUp :: Connection.Connection -> IO (Either Session.SessionError ())
-cleanUp connection = Session.run cleanUpSession connection
-  where
-    cleanUpSession :: Session.Session ()
-    cleanUpSession = Session.statement () cleanUpStatement
-    cleanUpStatement :: Statement.Statement () ()
-    cleanUpStatement = Statement.Statement rawSql E.noParams D.noResult True
-    rawSql = "truncate account"
-
-createAccountTable :: Statement.Statement () ()
+createAccountTable :: S.Statement () ()
 createAccountTable =
-  Statement.Statement s E.noParams D.noResult False
+  S.Statement s E.noParams D.noResult False
   where
     s = "create table account (id serial not null, balance numeric not null, primary key (id))"
 
-dropAccountTable :: Statement.Statement () ()
+dropAccountTable :: S.Statement () ()
 dropAccountTable =
-  Statement.Statement "drop table account" E.noParams D.noResult False
+  S.Statement "drop table account" E.noParams D.noResult False
 
-createAccount :: Statement.Statement Scientific Int64
+createAccount :: S.Statement Scientific Int64
 createAccount =
-  Statement.Statement
+  S.Statement
     "insert into account (balance) values ($1) returning id"
     ((E.param . E.nonNullable) E.numeric)
     (D.singleRow ((D.column . D.nonNullable) D.int8))
     True
 
-modifyBalance :: Statement.Statement (Int64, Scientific) Bool
+modifyBalance :: S.Statement (Int64, Scientific) Bool
 modifyBalance =
-  Statement.Statement
+  S.Statement
     "update account set balance = balance + $2 where id = $1"
     encode
     (fmap (> 0) D.rowsAffected)
@@ -73,9 +59,9 @@ modifyBalance =
         (E.param $ E.nonNullable E.int8)
         (E.param $ E.nonNullable E.numeric)
 
-getBalance :: Statement.Statement Int64 (Maybe Scientific)
+getBalance :: S.Statement Int64 (Maybe Scientific)
 getBalance =
-  Statement.Statement
+  S.Statement
     "select balance from account where id = $1"
     (E.param $ E.nonNullable E.int8)
     (D.rowMaybe ((D.column . D.nonNullable) D.numeric))
@@ -85,20 +71,20 @@ getBalance =
 -- Transaction
 ----------------------------------------------------------------------------------------------------
 
-createSchema :: Transaction.Transaction ()
-createSchema = Transaction.statement () createAccountTable
+createSchema :: T.Transaction ()
+createSchema = T.statement () createAccountTable
 
-dropSchema :: Transaction.Transaction ()
-dropSchema = Transaction.statement () dropAccountTable
+dropSchema :: T.Transaction ()
+dropSchema = T.statement () dropAccountTable
 
-transfer :: Int64 -> Int64 -> Scientific -> Transaction.Transaction Bool
+transfer :: Int64 -> Int64 -> Scientific -> T.Transaction Bool
 transfer id1 id2 amount = do
-  success <- Transaction.statement (id1, amount) modifyBalance
+  success <- T.statement (id1, amount) modifyBalance
   if success
-    then Transaction.statement (id2, negate amount) modifyBalance
+    then T.statement (id2, negate amount) modifyBalance
     else return False
 
-transferTimes :: Int -> Int64 -> Int64 -> Scientific -> Transaction.Transaction ()
+transferTimes :: Int -> Int64 -> Int64 -> Scientific -> T.Transaction ()
 transferTimes times id1 id2 amount =
   replicateM_ times $ transfer id1 id2 amount
 
@@ -106,24 +92,24 @@ transferTimes times id1 id2 amount =
 -- Test
 ----------------------------------------------------------------------------------------------------
 
-session :: Connection.Connection -> Session.Session a -> IO a
-session conn sess = Session.run sess conn >>= either (fail . show) return
+session :: C.Connection -> R.Session a -> IO a
+session conn sess = R.run sess conn >>= either (fail . show) return
 
-transaction :: Connection.Connection -> Transaction.Transaction a -> IO a
+transaction :: C.Connection -> T.Transaction a -> IO a
 transaction conn tran = session conn $ TS.transaction TS.RepeatableRead TS.Write tran
 
-type Test = Connection.Connection -> Connection.Connection -> IO Bool
+type Test = C.Connection -> C.Connection -> IO Bool
 
 transactionsTest :: Test
 transactionsTest conn1 conn2 = do
-  id1 <- session conn1 $ Session.statement 0 createAccount
-  id2 <- session conn1 $ Session.statement 0 createAccount
+  id1 <- session conn1 $ R.statement 0 createAccount
+  id2 <- session conn1 $ R.statement 0 createAccount
   async1 <- Async.async $ replicateM_ 1000 $ transaction conn1 $ transfer id1 id2 1
   async2 <- Async.async $ replicateM_ 1000 $ transaction conn2 $ transfer id1 id2 1
   Async.wait async1
   Async.wait async2
-  balance1 <- session conn1 $ Session.statement id1 getBalance
-  balance2 <- session conn1 $ Session.statement id2 getBalance
+  balance1 <- session conn1 $ R.statement id1 getBalance
+  balance2 <- session conn1 $ R.statement id2 getBalance
   traceShowM balance1
   traceShowM balance2
 
@@ -131,14 +117,14 @@ transactionsTest conn1 conn2 = do
 
 readAndWriteTransactionsTest :: Test
 readAndWriteTransactionsTest conn1 conn2 = do
-  id1 <- session conn1 $ Session.statement 0 createAccount
-  id2 <- session conn1 $ Session.statement 0 createAccount
+  id1 <- session conn1 $ R.statement 0 createAccount
+  id2 <- session conn1 $ R.statement 0 createAccount
   async1 <- Async.async $ replicateM_ 1000 $ transaction conn1 $ transfer id1 id2 1
-  async2 <- Async.async $ replicateM_ 1000 $ transaction conn2 $ Transaction.statement id1 getBalance
+  async2 <- Async.async $ replicateM_ 1000 $ transaction conn2 $ T.statement id1 getBalance
   Async.wait async1
   Async.wait async2
-  balance1 <- session conn1 $ Session.statement id1 getBalance
-  balance2 <- session conn1 $ Session.statement id2 getBalance
+  balance1 <- session conn1 $ R.statement id1 getBalance
+  balance2 <- session conn1 $ R.statement id2 getBalance
   traceShowM balance1
   traceShowM balance2
 
@@ -146,14 +132,14 @@ readAndWriteTransactionsTest conn1 conn2 = do
 
 transactionAndQueryTest :: Test
 transactionAndQueryTest conn1 conn2 = do
-  id1 <- session conn1 $ Session.statement 0 createAccount
-  id2 <- session conn1 $ Session.statement 0 createAccount
+  id1 <- session conn1 $ R.statement 0 createAccount
+  id2 <- session conn1 $ R.statement 0 createAccount
   async1 <- Async.async $ transaction conn1 $ transferTimes 200 id1 id2 1
-  async2 <- Async.async $ session conn2 $ replicateM_ 200 $ Session.statement (id1, 1) modifyBalance
+  async2 <- Async.async $ session conn2 $ replicateM_ 200 $ R.statement (id1, 1) modifyBalance
   Async.wait async1
   Async.wait async2
-  balance1 <- session conn1 $ Session.statement id1 getBalance
-  balance2 <- session conn1 $ Session.statement id2 getBalance
+  balance1 <- session conn1 $ R.statement id1 getBalance
+  balance2 <- session conn1 $ R.statement id2 getBalance
   traceShowM balance1
   traceShowM balance2
 
@@ -168,19 +154,19 @@ main = bracket mAcquire mRelease mUse
 
 ----------------------------------------------------------------------------------------------------
 
-mAcquire :: IO (Connection.Connection, Connection.Connection)
+mAcquire :: IO (C.Connection, C.Connection)
 mAcquire = (,) <$> acq <*> acq
   where
-    acq = join $ fmap (either (fail . show) return) $ Connection.acquire connectionSettings
-    connectionSettings = Connection.settings "localhost" 5432 "postgres" "1" "postgres"
+    acq = join $ fmap (either (fail . show) return) $ C.acquire cfg
+    cfg = C.settings "localhost" 5432 "postgres" "1" "postgres"
 
-mRelease :: (Connection.Connection, Connection.Connection) -> IO ()
+mRelease :: (C.Connection, C.Connection) -> IO ()
 mRelease (conn1, conn2) = do
   transaction conn1 dropSchema
-  Connection.release conn1
-  Connection.release conn2
+  C.release conn1
+  C.release conn2
 
-mUse :: (Connection.Connection, Connection.Connection) -> IO b
+mUse :: (C.Connection, C.Connection) -> IO b
 mUse (conn1, conn2) = do
   _ <- try (transaction conn1 dropSchema) :: IO (Either SomeException ())
   transaction conn1 createSchema
