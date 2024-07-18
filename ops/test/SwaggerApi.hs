41b5968ca40cddf693100866dd8565bedc134c49
jacob <jacobbishopxy@gmail.com>
2024-07-18 23:33:45 +0800

    :construction: `SwaggerDemo`

--- ops/test/SwaggerApi.hs
+++ ops/test/SwaggerApi.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
 
@@ -15,7 +15,7 @@ import Control.Lens
 import Data.Aeson (FromJSON, ToJSON, toJSON)
 import Data.Swagger
 import Data.Text (Text)
-import Data.Time (UTCTime (UTCTime), fromGregorian)
+import Data.Time (UTCTime (UTCTime), fromGregorian, getCurrentTime)
 import Data.Typeable (Typeable)
 import GHC.Generics (Generic)
 import Servant
@@ -25,10 +25,27 @@ todoAPI :: Proxy TodoAPI
 todoAPI = Proxy
 
 type TodoAPI =
-  "todo" :> Get '[JSON] [Todo]
-    :<|> "todo" :> ReqBody '[JSON] Todo :> Post '[JSON] TodoId
-    :<|> "todo" :> Capture "id" TodoId :> Get '[JSON] Todo
-    :<|> "todo" :> Capture "id" TodoId :> ReqBody '[JSON] Todo :> Put '[JSON] TodoId
+  "time" :> Summary "time" :> Get '[PlainText] String
+    :<|> "todo"
+      :> Summary "retrieve all todos"
+      :> Get '[JSON] [Todo]
+    :<|> "todo"
+      :> Summary "get a todo"
+      :> Capture "id" TodoId
+      :> Get '[JSON] Todo
+    :<|> "todo"
+      :> Summary "save a todo"
+      :> ReqBody '[JSON] Todo
+      :> Post '[JSON] TodoId
+    :<|> "todo"
+      :> Summary "update a todo by :id"
+      :> Capture "id" TodoId
+      :> ReqBody '[JSON] Todo
+      :> Put '[JSON] Todo
+    :<|> "todo"
+      :> Summary "delete a todo by :id"
+      :> Capture "id" TodoId
+      :> Delete '[JSON] Todo
 
 type SwapperAPI =
   "swagger.json" :> Get '[JSON] Swagger
@@ -36,35 +53,55 @@ type SwapperAPI =
 type API = SwapperAPI :<|> TodoAPI
 
 data Todo = Todo
-  { created :: UTCTime,
-    summary :: Text
+  { brief :: Text,
+    contents :: Text,
+    priority :: Int,
+    created :: Maybe UTCTime,
+    modified :: Maybe UTCTime
   }
-  deriving (Show, Generic, Typeable)
-
-newtype TodoId = TodoId Int
-  deriving (Show, Generic, Typeable, ToJSON, FromHttpApiData)
-
-instance ToJSON Todo
-
-instance FromJSON Todo
+  deriving (Show, Generic, Typeable, ToJSON, FromJSON)
 
 instance ToSchema Todo where
   declareNamedSchema proxy =
     genericDeclareNamedSchema defaultSchemaOptions proxy
       & mapped . schema . description ?~ "This is some real Todo right here"
-      & mapped . schema . example ?~ toJSON (Todo (UTCTime (fromGregorian 2024 7 15) 0) "get milk")
+      & mapped . schema . example ?~ toJSON mockTodo
 
-instance ToParamSchema TodoId
+newtype TodoId = TodoId Int
+  deriving (Show, Generic, Typeable, ToJSON, ToParamSchema, ToSchema)
 
-instance ToSchema TodoId
+newTodo :: Text -> Text -> Int -> IO Todo
+newTodo b c p = do
+  t <- getCurrentTime
+  let t' = Just t
+  return $ Todo b c p t' t'
+
+mockTodo :: Todo
+mockTodo =
+  Todo "get milk" "goto grocery store and buy a bottle of milk" 2 t t
+  where
+    t = Just $ UTCTime (fromGregorian 2024 7 15) 0
+
+modifyTodo :: Todo -> Todo -> IO Todo
+modifyTodo newT todo = do
+  getCurrentTime >>= \t ->
+    return $
+      todo
+        { brief = brief newT,
+          contents = contents newT,
+          priority = priority newT,
+          modified = Just t
+        }
+
+----------------------------------------------------------------------------------------------------
 
 todoSwagger :: Swagger
 todoSwagger =
   toSwagger todoAPI
     & info . title .~ "Todo API"
-    & info . version .~ "0.0"
+    & info . version .~ "0.0.1"
     & info . description ?~ "This is an API that tests swagger integration"
-    & info . license ?~ ("MIT" & url ?~ URL "http://mit.com")
+    & info . license ?~ ("BSD 3.0" & url ?~ URL "https://opensource.org/licenses/BSD-3-Clause")
 
 swaggerServer :: Server API
 swaggerServer = return todoSwagger :<|> error "not implemented"
--- ops/test/SwaggerDemo.hs
+++ ops/test/SwaggerDemo.hs
@@ -12,14 +12,41 @@ module Main where
 
 import Control.Monad.IO.Class (liftIO)
 import Data.IORef
+import Data.Map
 import Data.Swagger
-import Data.Text
 import Data.Time
 import Network.Wai
 import Network.Wai.Handler.Warp (run)
 import Servant
 import Servant.Swagger.UI
 import SwaggerApi
+
+----------------------------------------------------------------------------------------------------
+
+getTime :: Handler String
+getTime = liftIO getCurrentTime >>= return . formatTime defaultTimeLocale "%FT%T%QZ"
+
+type TodoStore = Map TodoId Todo
+
+getTodos :: IORef TodoStore -> Handler [Todo]
+getTodos = undefined
+
+getTodo :: IORef TodoStore -> TodoId -> Handler Todo
+getTodo = undefined
+
+postTodo :: IORef TodoStore -> TodoId -> Handler TodoId
+postTodo = undefined
+
+putTodo :: IORef TodoStore -> TodoId -> Todo -> Handler Todo
+putTodo = undefined
+
+deleteTodo :: IORef TodoStore -> TodoId -> Handler Todo
+deleteTodo = undefined
+
+----------------------------------------------------------------------------------------------------
+
+todoServer :: Server TodoAPI
+todoServer = undefined
 
 main :: IO ()
 main = do
